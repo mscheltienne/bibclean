9b3d24ca234ad2a6912b95c1455deda11b54c344
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-10-12 18:15:06 +0200

    add a util function to retrieve the entry type

---
+++ bibclean/utils/utils.py
@@ -0,0 +1,22 @@
+from typing import Dict
+
+from bibtexparser.bibdatabase import BibDatabase
+
+
+def get_entry_types(bib_database: BibDatabase) -> Dict[str, str]:
+    """Retrieve the entry type from a BibTex database.
+
+    Parameters
+    ----------
+    bib_database : BibDatabase
+        BibTex database.
+
+    Returns
+    -------
+    entry_types : dict
+        A dictionary with the cite key as key and the type as value.
+    """
+    return {
+        cite_key: entry["ENTRYTYPE"]
+        for cite_key, entry in bib_database.entries_dict.items()
+    }
