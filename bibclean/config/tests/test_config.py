4fccd781abdcd2d47ae5760842433c4994a78e53
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-10-13 12:12:28 +0200

    fix tests for config loader

--- bibclean/config/tests/test_config.py
+++ bibclean/config/tests/test_config.py
@@ -3,11 +3,21 @@ from bibclean.config import _load_default_config
 
 def test_load_default_config():
     """Test loading of the default configuration."""
-    required_fields = _load_default_config()
+    required_fields, keep_fields = _load_default_config()
     assert isinstance(required_fields, dict)
+    assert isinstance(keep_fields, dict)
     assert "article" in required_fields
+    assert "article" in keep_fields
     # check article
     assert "author" in required_fields["article"]
     assert "journal" in required_fields["article"]
     assert "title" in required_fields["article"]
     assert "year" in required_fields["article"]
+    assert "author" in keep_fields["article"]
+    assert "journal" in keep_fields["article"]
+    assert "month" in keep_fields["article"]
+    assert "number" in keep_fields["article"]
+    assert "pages" in keep_fields["article"]
+    assert "title" in keep_fields["article"]
+    assert "volume" in keep_fields["article"]
+    assert "year" in keep_fields["article"]
