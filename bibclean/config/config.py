368662835a9f0de3f2296e9f9c889ec320db0b3d
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-10-13 11:54:41 +0200

    add fields to keep to the configuration

--- bibclean/config/config.py
+++ bibclean/config/config.py
@@ -1,15 +1,16 @@
 from configparser import ConfigParser
 from pathlib import Path
-from typing import Dict, Set
+from typing import Dict, Set, Tuple
 
 
-def _load_default_config() -> Dict[str, Set[str]]:
+def _load_default_config() -> Tuple[Dict[str, Set[str]], Dict[str, Set[str]]]:
     """Load the default config from 'default.ini'."""
     config = ConfigParser()
     config.optionxform = str
     config.read(Path(__file__).parent / "default.ini")
 
     required_fields = dict()
+    keep_fields = dict()
     for key in config.keys():
         if key == "DEFAULT":
             continue
@@ -22,13 +23,24 @@ def _load_default_config() -> Dict[str, Set[str]]:
                 "missing. Please contact the developers on GitHub."
             )
 
+        try:
+            keep = config[key]["keep"]
+        except KeyError:  # sanity-check
+            raise KeyError(
+                "'.ini' default file is invalid. The 'keep' key is "
+                "missing. Please contact the developers on GitHub."
+            )
+
         req = [elt for elt in req.split("\n") if len(elt) != 0]
-        if len(req) != len(set(req)):  # sanity-check
+        keep = [elt for elt in keep.split("\n") if len(elt) != 0]
+        if len(req) != len(set(req)) or len(keep) != len(set(keep)):
             raise ValueError(
                 "'.ini' default file is invalid. Some fields are present "
                 "multiple times for the same entry type. Please contact the "
                 "developers on GitHub."
             )
-        required_fields[key] = set(req)
 
-    return required_fields
+        required_fields[key] = set(req)
+        keep_fields[key] = set(keep)
+
+    return required_fields, keep_fields
