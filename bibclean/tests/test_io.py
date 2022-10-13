321176833ec7e88fa4c1066539b54a97d24884e7
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-10-13 15:23:56 +0200

    test i/o

--- bibclean/tests/test_io.py
+++ bibclean/tests/test_io.py
@@ -1,11 +1,16 @@
+from pathlib import Path
+
 import pytest
+from bibtexparser.bibdatabase import BibDatabase
 
 from bibclean.io import load_bib, save_bib
 
 
 def test_load_bib():
     """Test loading of BibTex files."""
-    pass
+    file = Path(__file__).parent / "data" / "zotero-articles.bib"
+    bib_database = load_bib(file)
+    assert isinstance(bib_database, BibDatabase)
 
 
 def test_load_bib_invalid_args():
@@ -25,7 +30,25 @@ def test_load_bib_invalid_args():
 
 def test_save_bib(tmp_path):
     """Test saving of BibTex files."""
-    pass
+    file = Path(__file__).parent / "data" / "zotero-articles.bib"
+    bib_database = load_bib(file)
+
+    # save/reload
+    save_bib(bib_database, tmp_path / "101.bib")
+    bib_database_loaded = load_bib(tmp_path / "101.bib")
+    assert all(elt in bib_database_loaded.entries for elt in bib_database.entries)
+    assert all(elt in bib_database.entries for elt in bib_database_loaded.entries)
+
+    # overwrite
+    with pytest.raises(IOError, match="The provided file already exist."):
+        save_bib(bib_database, tmp_path / "101.bib")
+    save_bib(bib_database, tmp_path / "101.bib", overwrite=True)
+
+    # encoding
+    save_bib(bib_database, tmp_path / "101-2.bib", encoding="utf-16")
+    bib_database_loaded = load_bib(tmp_path / "101-2.bib", encoding="utf-16")
+    assert all(elt in bib_database_loaded.entries for elt in bib_database.entries)
+    assert all(elt in bib_database.entries for elt in bib_database_loaded.entries)
 
 
 def test_save_bib_invalid_args():
@@ -34,3 +57,11 @@ def test_save_bib_invalid_args():
         TypeError, match="'bib_database' must be an instance of BibDatabase"
     ):
         save_bib(101, "101.bib")
+
+    file = Path(__file__).parent / "data" / "zotero-articles.bib"
+    bib_database = load_bib(file)
+    with pytest.raises(
+        IOError,
+        match="The provided file extension is not '.bib'. '.101' is invalid.",
+    ):
+        save_bib(bib_database, "101.101")
