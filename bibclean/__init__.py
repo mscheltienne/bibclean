2743205230219152d7f34bc26a7dfb367f74c13c
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2023-03-21 20:15:34 +0100

    update utilities

--- bibclean/__init__.py
+++ bibclean/__init__.py
@@ -1,11 +1,5 @@
 from ._version import __version__  # noqa: F401
 from .check import check_bib_database  # noqa: F401
 from .clean import clean_bib_database  # noqa: F401
-from .utils._config import sys_info  # noqa: F401
-from .utils._logs import (  # noqa: F401
-    add_file_handler,
-    add_stream_handler,
-    logger,
-    set_handler_log_level,
-    set_log_level,
-)
+from .utils.config import sys_info  # noqa: F401
+from .utils.logs import add_file_handler, logger, set_log_level  # noqa: F401
