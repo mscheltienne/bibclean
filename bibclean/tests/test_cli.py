f91b47442500620c40fcc8e537fadf8ab0e35004
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-10-24 15:37:01 +0200

    fix tests

--- bibclean/tests/test_cli.py
+++ bibclean/tests/test_cli.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from bibclean.commands.bibclean_check import _run
+from bibclean.commands.bibclean_check import ReturnCode, _run
 
 
 def test_run():
@@ -9,21 +9,21 @@ def test_run():
 
     # already valid
     exit_code = _run(directory / "zotero-clean.bib", None)
-    assert exit_code == 0
+    assert exit_code == ReturnCode.no_violations_found
 
     # in-existant config file
     exit_code = _run(directory / "zotero-clean.bib", config="101.toml")
-    assert exit_code == 2
+    assert exit_code == ReturnCode.invalid_options
 
     # invalid
     exit_code = _run(directory / "zotero-articles.bib", None)
-    assert exit_code == 1
+    assert exit_code == ReturnCode.violations_found
     exit_code = _run(directory / "zotero-duplicates.bib", None)
-    assert exit_code == 1
+    assert exit_code == ReturnCode.violations_found_unfixable
 
     # invalid but with excluded elements, thus valid
     exit_code = _run(
         directory / "zotero-duplicates.bib",
         str(directory / "zotero-duplicates.toml"),
     )
-    assert exit_code == 0
+    assert exit_code == ReturnCode.no_violations_found
